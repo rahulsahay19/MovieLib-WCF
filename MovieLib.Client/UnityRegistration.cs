5b295ff60c9932bcb7118911fc23274ea9b77f6f
rahul sahay <rahulsahay19@gmail.com>
2017-02-27 17:50:46 +0530

    Updated Unity Registration with refactoring

--- MovieLib.Client/UnityRegistration.cs
+++ MovieLib.Client/UnityRegistration.cs
@@ -6,24 +6,45 @@ using System.Reflection;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
 using System.ServiceModel.Configuration;
+using System.ServiceModel.Description;
 using Microsoft.Practices.Unity;
+using MovieLib.Corporate.Core.Service;
+using MovieLib.Corporate.Core.Service.Configuration;
+using MovieLib.Corporate.Core.Wcf.Behaviours;
+using MovieLib.Portal.Shell.Infrastructure.Common.Context.Wcf;
 
-namespace Nasdaq.Corporate.Portal.IR.Api
+namespace MovieLib.Portal.Shell.Api
 {
     /// <summary>
     /// Specifies the Unity configuration for the main container.
     /// </summary>
     public class UnityConfig
     {
-        #region Unity Container
+        /// <summary>
+        /// Configuration section name.
+        /// </summary>
+        private const string ConfigurationSection = "serviceConfiguration";
 
+        private const string ChannelWcf = "wcf";
+        private const string ChannelInproc = "inproc";
+
+        /// <summary>
+        /// List of all available in-proc services
+        /// </summary>
+        private static Dictionary<string, IService> inProcServices = new Dictionary<string, IService>();
+
+        /// <summary>
+        /// List of all available wcf services
+        /// </summary>
+        private static HashSet<string> wcfServices = new HashSet<string>();
+
+        #region Unity Container
         private static Lazy<IUnityContainer> container = new Lazy<IUnityContainer>(() =>
         {
             var container = new UnityContainer();
             RegisterTypes(container);
             return container;
         });
-
 
         /// <summary>
         /// Gets the configured Unity container.
@@ -32,7 +53,6 @@ namespace Nasdaq.Corporate.Portal.IR.Api
         {
             return container.Value;
         }
-
         #endregion
 
         /// <summary>Registers the type mappings with the Unity container.</summary>
@@ -46,6 +66,8 @@ namespace Nasdaq.Corporate.Portal.IR.Api
 
             // TODO: Register your types here
 
+            //for in proc
+            // container.RegisterType<IMarketDataServiceAsync>().
             /*Working Piece inline*/
             /*container.RegisterType<IContactServiceAsync>(
                 new TransientLifetimeManager(),
@@ -53,33 +75,99 @@ namespace Nasdaq.Corporate.Portal.IR.Api
                     (c) => new ChannelFactory<IContactServiceAsync>("IContactService").CreateChannel()));*/
 
             // LoadAssembly(container);
+
+            // NavigationLayouts.ResolveProperty = () => container.Resolve<INavigationServiceAsync>();
+            /*container.RegisterType<ShellContextHeaderClientBehavior>();*/
+
             CreateFactory(container);
         }
 
-
         private static void CreateFactory(IUnityContainer container)
         {
-            Type t = null;
-            Assembly assembly = Assembly.Load("MovieLib.Portal.Data.Contract");
-            foreach (Type ti in assembly.GetTypes().Where(x => x.IsInterface))
+            //For inproc, Below method is just there to isolate the inproc stuff from wcf registration
+            //In-Proc Calls shouldn't be part of Unity Registration.
+            CheckInProc();
+
+            var clientSection = GetClientSection();
+
+            //Logic to read the endpoints and construct assembly on the fly
+            for (int i = 0; i < clientSection.Endpoints.Count; i++)
             {
-                if (ti.GetCustomAttributes(true).OfType<ServiceContractAttribute>().Any())
+                var contract = clientSection.Endpoints[i].Contract;
+                var typeParts = contract.Split('.');
+                var constructAssembly = "";
+                var endPointName = clientSection.Endpoints[i].Name;
+
+                //This logic is there to skip N3A API, as this won't be needed for registration process.
+                if (endPointName != "IAuthenticationSessionApi")
                 {
-                    var incomingInterface = ti;
-
-                    //For debugging Mapping Invoke that factory exclusively, as this won't step through from 
-                    //Unity unless any exception occurs
-
-                    // CreateGenericFactory(incomingInterface);
-
-                    container.RegisterType(incomingInterface,
-                       new TransientLifetimeManager(),
-                       new InjectionFactory(
-                         c => CreateGenericFactory(incomingInterface)
-                       ));
+                    for (int j = 0; j < typeParts.Length - 1; j++)
+                    {
+                        constructAssembly += typeParts[j] + ".".Trim();
+                    }
+                    constructAssembly = constructAssembly.TrimEnd('.');
                 }
+
+               
+                Assembly inassembly = null;
+                if (constructAssembly != "")
+                {
+                    /*TODO:- Below stuff is temprorary*/
+                    if (constructAssembly != "RequestReduce.SqlServer.Data.Service.Contract")
+                    {
+                        inassembly = Assembly.Load(constructAssembly);
+                    }
+                }
+                if (inassembly != null)
+                    foreach (Type ti in inassembly.GetTypes().Where(x => x.IsInterface))
+                    {
+                        if (ti.GetCustomAttributes(true).OfType<ServiceContractAttribute>().Any())
+                        {
+                            var incomingInterface = ti;
+
+                            //For debugging Mapping Invoke that factory exclusively, as this won't step through from 
+                            //Unity unless any exception occurs
+
+                            //  CreateGenericFactory(incomingInterface);
+
+                            container.RegisterType(incomingInterface,
+                                new TransientLifetimeManager(),
+                                new InjectionFactory(
+                                    c => CreateGenericFactory(incomingInterface)
+                                    ));
+                        }
+                    }
             }
 
+        }
+
+        //Method to check whether in proc or not. 
+        //TODO:- This will actually come in picture in future, when In-Procs services will be there.
+        private static bool CheckInProc()
+        {
+            var configSection = ConfigurationManager.GetSection(ConfigurationSection) as ServiceConfigurationSection;
+            bool inProcFlag = false;
+            if (configSection != null)
+            {
+                var svcConfig = configSection.Services;
+                foreach (ServiceConfigurationElement config in svcConfig)
+                {
+                    if (config.IsEnabled)
+                    {
+                        if (config.Channel == ChannelInproc)
+                        {
+                            //If channel is inproc, load the assembly, create the instance and add to the list for caching.
+                            var typeParts = config.Type.Split(',');
+                            var assemblyName = typeParts[1].Trim();
+                            var inassembly = Assembly.Load(assemblyName);
+                            var service = inassembly.CreateInstance(typeParts[0].Trim()) as IService;
+
+                            inProcFlag = true;
+                        }
+                    }
+                }
+            }
+            return inProcFlag;
         }
 
         private static object CreateGenericFactory(Type t)
@@ -88,26 +176,19 @@ namespace Nasdaq.Corporate.Portal.IR.Api
             Type factoryType = typeof(ChannelFactory<>);
             factoryType = factoryType.MakeGenericType(t);
 
-
             //Call the Binding section to get the associtaed binding
             var section = GetBindingsSection();
+
             Binding binding = null;
             string bindingName = null;
 
-            List<Uri> addressCollection = new List<Uri>();
-            List<string> contractCollection = new List<string>();
-            List<string> bindinCollection = new List<string>();
-            object address = null;
+            List<string> contractCollection;
+            object address;
+            ClientSection clientSection;
 
-            ClientSection clientSection = (ClientSection)ConfigurationManager.GetSection("system.serviceModel/client");
-            for (int j = 0; j < clientSection.Endpoints.Count; j++)
-            {
-                addressCollection.Add(clientSection.Endpoints[j].Address);
-                contractCollection.Add(clientSection.Endpoints[j].Contract);
-                bindinCollection.Add(clientSection.Endpoints[j].Binding);
-            }
+            var addressCollection = ApplyAddressCollection(out contractCollection, out address, out clientSection);
 
-
+            //Loop through the address collection and check for required contract
             for (int k = 0; k < addressCollection.Count; k++)
             {
                 //Check for contract, if found, then pull corresponding address
@@ -116,32 +197,76 @@ namespace Nasdaq.Corporate.Portal.IR.Api
                     address = addressCollection[k].AbsoluteUri;
                     bindingName = clientSection.Endpoints[k].Binding;
                     //Now loop through collected binding name and check for configured bindings
-                    foreach (var bindingCollection in section.BindingCollections)
-                    {
-                        if (bindingCollection.BindingName.Equals(bindingName))
-                        {
-
-                            for (int i = 0; i < bindingCollection.ConfiguredBindings.Count; i++)
-                            {
-                                if (bindingCollection.ConfiguredBindings.Count > 0)
-                                {
-                                    var bindingElement = bindingCollection.ConfiguredBindings[i];
-                                    binding = (Binding)Activator.CreateInstance(bindingCollection.BindingType);
-                                    binding.Name = bindingElement.Name;
-                                    bindingElement.ApplyConfiguration(binding);
-                                }
-                            }
-                        }
-                    }
+                    binding = ApplyBindingConfiguration(section, bindingName, binding);
                 }
             }
 
             var factory = Activator.CreateInstance(factoryType, binding, address);
 
+            ApplyEndPointBehaviors(factory);
+
             MethodInfo method = factoryType.GetMethod("CreateChannel", new Type[] { });
 
             return method.Invoke(factory, null);
         }
+
+        //Method to Apply Endpoint behaviors 
+        private static void ApplyEndPointBehaviors(object factory)
+        {
+            ((ChannelFactory)factory).Endpoint.Behaviors.Add(new PortalContextHeaderClientBehavior());
+            ((ChannelFactory)factory).Endpoint.Behaviors.Add(new CorpFaultBehavior());
+        }
+
+        //Method to collect all EndPoint addresses
+        private static List<Uri> ApplyAddressCollection(out List<string> contractCollection, out object address,
+            out ClientSection clientSection)
+        {
+            List<Uri> addressCollection = new List<Uri>();
+            contractCollection = new List<string>();
+            List<string> bindinCollection = new List<string>();
+            address = null;
+
+            clientSection = GetClientSection();
+
+            for (int j = 0; j < clientSection.Endpoints.Count; j++)
+            {
+                addressCollection.Add(clientSection.Endpoints[j].Address);
+                contractCollection.Add(clientSection.Endpoints[j].Contract);
+                bindinCollection.Add(clientSection.Endpoints[j].Binding);
+            }
+            return addressCollection;
+        }
+
+        //Method to apply the Binding Collection
+        private static Binding ApplyBindingConfiguration(BindingsSection section, string bindingName, Binding binding)
+        {
+            foreach (var bindingCollection in section.BindingCollections)
+            {
+                if (bindingCollection.BindingName.Equals(bindingName))
+                {
+                    for (int i = 0; i < bindingCollection.ConfiguredBindings.Count; i++)
+                    {
+                        if (bindingCollection.ConfiguredBindings.Count > 0)
+                        {
+                            var bindingElement = bindingCollection.ConfiguredBindings[i];
+                            binding = (Binding)Activator.CreateInstance(bindingCollection.BindingType);
+                            binding.Name = bindingElement.Name;
+                            bindingElement.ApplyConfiguration(binding);
+                        }
+                    }
+                }
+            }
+            return binding;
+        }
+
+        //Method to fetch Client Section
+        private static ClientSection GetClientSection()
+        {
+            ClientSection clientSection = (ClientSection)ConfigurationManager.GetSection("system.serviceModel/client");
+            return clientSection;
+
+        }
+
 
         //This will return all the bindings from the config file
         private static BindingsSection GetBindingsSection()
@@ -152,6 +277,4 @@ namespace Nasdaq.Corporate.Portal.IR.Api
         }
 
     }
-
-
 }
